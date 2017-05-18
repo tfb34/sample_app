b07abca5249f3fc968960ef692f1629e50bfc3da
tfb34 <britothalia212@gmail.com>
2017-05-18 18:13:29 -0400

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
   	if user && user.authenticate(params[:session][:password])
   		#log the user in and redirect to the user's show page
   		log_in user 
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
   		redirect_to user# rails auto. converts this to user_url(user)
   	else
   		#create an error message
@@ -17,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
